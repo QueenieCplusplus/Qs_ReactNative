387e39e0043a03b62aa815cb3d4a26cad59631b2
Queenie Chen <58072875+QueenieCplusplus@users.noreply.github.com>
2020-07-20 11:07:06 +0800

    Update App.js

--- qsJSXapp/App.js
+++ qsJSXapp/App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Kate is back, enjoy to code for app.</Text>
+        <Button  title="click me" accessibilityLabel="OK"> </Button>
+        <Button  title="contact me" accessibilityLabel="OK"> </Button>
     </View>
   );
 }
